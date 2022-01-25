3531ebb837e81167d0b273e79b2f7f646966f08d
M-Shinoda <sect011masami@gmail.com>
2022-01-25 19:20:30 +0900

    change support hooks and riverpod for main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,54 +1,62 @@
 import 'dart:async';
-import 'dart:io';
+// import 'dart:io';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 // ignore: import_of_legacy_library_into_null_safe
-import 'package:fluttube/list_card.dart';
-import 'package:fluttube/share_receive_url.dart';
-import 'downloader.dart';
-// import 'share_receive_url.dart';
+// import 'package:fluttube/list_card.dart';
+// import 'package:fluttube/share_receive_url.dart';
+import 'package:receive_sharing_intent/receive_sharing_intent.dart';
+// import 'downloader.dart';
+// import 'share_receive_url.dart';]
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    ProviderScope(
+      child: MyApp(),
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MyApp extends HookWidget {
   @override
   Widget build(BuildContext context) {
+    useEffect(() {
+      late StreamSubscription _intentDataStreamSubscription;
+      // 起動中
+      _intentDataStreamSubscription =
+          ReceiveSharingIntent.getTextStream().listen((String value) async {
+        print("Shared: $value");
+      }, onError: (err) {
+        print("getLinkStream error: $err");
+      });
+
+      // 停止中
+      ReceiveSharingIntent.getInitialText().then((String? value) async {
+        if (value == null) return;
+        print("Shared Backgraund: $value");
+      });
+    });
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blueGrey,
       ),
-      home: const MyHomePage(title: 'Fluttube'),
+      home: demoPage(),
     );
   }
-}
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  List<Map<String, dynamic>> items = [];
-
-  int _counter = 0;
-
-  @override
-  Widget build(BuildContext context) {
+  Widget demoPage() {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: const Text('Fluttube'),
         actions: <Widget>[
           IconButton(
-            onPressed: () => download(context),
+            onPressed: () {
+              print('touch download icon');
+            },
             icon: const Icon(CupertinoIcons.cloud_download),
           )
         ],
@@ -58,26 +66,17 @@ class _MyHomePageState extends State<MyHomePage> {
           // mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             // リストビュー
-            Expanded(child: ListCard(items: items)),
+            Expanded(child: Container()),
             FloatingActionButton(
               // onPressedでボタンが押されたらテキストフィールドの内容を取得して、アイテムに追加
-              onPressed: () async {
-                final title = await download(context);
-                _addItem(title);
+              onPressed: () {
+                print('touch floating button');
               },
               child: const Icon(Icons.add),
             ),
-            ShareReceiveUrl()
           ],
         ),
       ),
     );
-  }
-
-  void _addItem(String title) {
-    setState(() {
-      _counter++;
-      items.add({"id": _counter, "title": title});
-    });
   }
 }
