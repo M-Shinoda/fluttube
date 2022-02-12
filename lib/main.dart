71f8f37c5abbdef20f9eeaa8e2d29b1a0c0e8a68
M-Shinoda <sect011masami@gmail.com>
2022-02-12 15:16:33 +0900

    remove statement

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,7 @@ class MyApp extends HookConsumerWidget {
     useEffect(() {
       sharingUrlProc(dListNotifier);
     }, []);
+
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: BottomView(),
