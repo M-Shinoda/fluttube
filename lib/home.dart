71eaccbe7ac93b97af97d9c8a766093a78267290
M-Shinoda <sect011masami@gmail.com>
2022-02-12 16:14:03 +0900

    remove unuse statement

--- lib/home.dart
+++ lib/home.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:fluttube/count.dart';
 import 'package:fluttube/home_body.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'home_body.dart';
@@ -10,20 +9,9 @@ class HomeView extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final count = ref.watch(countProvider);
-
     return Scaffold(
       appBar: AppBar(
         title: const Text('Fluttube '),
-        actions: <Widget>[
-          IconButton(
-            onPressed: () {
-              // ignore: avoid_print
-              print('$count');
-            },
-            icon: const Icon(CupertinoIcons.cloud_download),
-          )
-        ],
       ),
       body: const HomeBodyView(),
     );
