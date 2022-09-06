17a5ec208519b3d832f0adb308178d405aefd6ab
M-Shinoda <sect011masami@gmail.com>
2022-09-06 11:31:19 +0900

    refactoring

--- lib/download_list.dart
+++ lib/download_list.dart
@@ -36,34 +36,15 @@ class DownloadListStateNotifier extends StateNotifier<List<UrlState>> {
     var yt = YoutubeExplode();
     var id = VideoId(status[index].url);
     var video = await yt.videos.get(id);
-    // ignore: avoid_print
-    print("Title: ${video.title}");
-    // ignore: avoid_print
-    print("Title: ${video.duration}");
     await Permission.storage.request();
     var manifest = await yt.videos.streamsClient.getManifest(id);
     var audio = manifest.audioOnly.firstWhere((item) => item.tag == 140);
     var bytes = audio.size.totalBytes;
     var fileName = _composeFileName(video.title);
     print(fileName);
-    var filePath =
-        // path.join(dirM.path, '${video.id}.${audio.container.name}');
-        path.join(dirM.path, fileName);
+    var filePath = path.join(dirM.path, fileName);
     var file = File(filePath);
-    // ignore: avoid_print
-    print(file);
     var fileStream = file.openWrite();
-
-    // Pipe all the content of the stream into our file.
-    // await yt.videos.streamsClient.get(audio).pipe(fileStream);
-    /*
-                  If you want to show a % of download, you should listen
-                  to the stream instead of using `pipe` and compare
-                  the current downloaded streams to the totalBytes,
-                  see an example ii example/video_download.dart
-                   */
-
-    // Close the file.
 
     var audioStream = yt.videos.streamsClient.get(audio);
     var count = 0;
